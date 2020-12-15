ea98295d0265bd763da6c6f44599bcba1caccd47
agavriliu17 <alin_gavriliu@yahoo.com>
2020-12-15 16:26:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <mmsystem.h>
 using namespace std;
 #define N 3
+#define CLIP_ON 1
 
 // rgb colors
 int black[3] = {0,0,0};
@@ -17,6 +18,7 @@ int screenHeight = 684;
 int screen_poly[8] = {0,0, screenWidth,0, screenWidth,screenHeight, 0,screenHeight}; //lista ce determina coordonatele poligonului ecranului
 bool gameOpen = true;
 int mouseX, mouseY; //coordonatele mouse-ului
+int pressed=0; //variabila pentru butoane din diferite meniuri
 
 void hide_matrix(int a[][3],int b[][3]){//logica pentru suprapunerea matricelor
     for(int i=0;i<3;i++){
@@ -73,6 +75,51 @@ void menu(){ //plaseaza fundalul si textul ferestrei de meniu
 
 }
 
+void levels(){ //pagina pentru selectarea gradului de dificultate a jocului
+    readimagefile("menu_background.jpg",0,0,screenWidth,screenHeight);
+    setfillstyle(SOLID_FILL, COLOR(152, 65, 21));
+    settextstyle(BOLD_FONT, HORIZ_DIR, 5);
+    setbkcolor(COLOR(sea_blue[0],sea_blue[1],sea_blue[2]));
+
+    //how to play
+
+    bar(534, 257, 865, 311);
+    outtextxy(700-textwidth("HOW TO PLAY")/2, 284-textheight("H")/2, "HOW TO PLAY");
+
+    settextstyle(BOLD_FONT, HORIZ_DIR, 5);
+    //starter
+
+    bar(535, 372, 866, 427);
+    outtextxy(700-textwidth("STARTER")/2, 400-textheight("S")/2, "STARTER");
+
+
+    //junior
+    bar(535, 372+70, 866, 427+70);
+    outtextxy(700-textwidth("JUNIOR")/2, 400+70-textheight("J")/2, "JUNIOR");
+
+    //expert
+
+    bar(535, 372+140, 866, 427+140);
+    outtextxy(700-textwidth("EXPERT")/2, 400+140-textheight("E")/2, "EXPERT");
+
+    //master
+
+    bar(535, 372+210, 866, 427+210);
+    outtextxy(700-textwidth("MASTER")/2, 400+210-textheight("M")/2, "MASTER");
+
+    //back to menu
+
+    bar(307, 609, 452, 664);
+    outtextxy(379-textwidth("BACK")/2, 636-textheight("B")/2, "BACK");
+    getmouseclick(WM_LBUTTONDOWN,mouseX,mouseY);
+    mouseX=mousex();
+    mouseY=mousey();
+    if(mouseX > 535 && mouseX < 866 && mouseY > 372+140 && mouseY < 427+140){
+        cout<<mouseX<<" "<<mouseY;
+        menu();
+    }
+}
+
 int settings(){ //plaseaza fundalul si textul ferestrei setari
     bool settingsOpen = true;
     while(settingsOpen){
@@ -82,11 +129,22 @@ int settings(){ //plaseaza fundalul si textul ferestrei setari
 }
 bool startGame(){ // ciclul principal al jocului unde are loc procesarea logicii
     while(gameOpen){
-        PlaySound(TEXT("pirates.wav"), NULL, SND_FILENAME); //va fi mutat intr o functie ce va fi apelata in setari
+       // PlaySound(TEXT("pirates.wav"), NULL, SND_FILENAME); va fi mutat intr o functie ce va fi apelata in setari
+       // primul meniu
         getmouseclick(WM_LBUTTONDOWN,mouseX,mouseY);
-        if(mouseX > (screenWidth/2 - 200) && mouseX < (screenWidth/2 +200) && mouseY > (screenHeight/4 + 360) && mouseY < (screenHeight/4 + 440)){
+        if(mouseX > 575 && mouseX < 829 && mouseY > 320 && mouseY < 392 && pressed==0){
+            clearmouseclick(WM_LBUTTONDOWN);
+            pressed=1;
+            levels();
+        }
+        if(mouseX > (screenWidth/2 - 200) && mouseX < (screenWidth/2 +200) && mouseY > (screenHeight/4 + 360) && mouseY < (screenHeight/4 + 440) && pressed==0){
             clearmouseclick(WM_LBUTTONDOWN);
             gameOpen = false;
+        }
+        // nivele
+        if(mouseX > 307 && mouseX < 452 && mouseY > 609 && mouseY < 664 && pressed==1){
+        pressed=0;
+        menu();
         }
 
     }
